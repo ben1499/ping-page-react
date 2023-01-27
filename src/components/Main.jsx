0d288604a66b59507053d81d4fd32a166463110c
Reuben <reubenthomas99@gmail.com>
2023-01-27 19:56:20 +0530

    Add mobile responsive

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Main() {
+    const [inputText, setInputText] = useState("");
+    const [infoText, setInfoText] = useState("");
+
+    function handleChange(event) {
+        const text = event.target.value;
+        setInputText(text);
+    }
+
+    function handleClick(event) {
+        event.preventDefault();
+        let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if(!regex.test(inputText)) {
+            setInfoText("Please provide a valid email address");
+        } else {
+            setInfoText("");
+        }
+    }
+
+
     return (
         <main>
             <div className="title-text">
@@ -8,9 +27,11 @@ function Main() {
                 <p>Subscribe and get notified</p>
             </div>
             <form className="main-form" action="">
-                <input className="cus-input" type="text" placeholder="Your email address..."/>
-                <button className="btn-cus" type="submit">Notify Me</button>
+                <input className="cus-input" onChange={handleChange} value={inputText} type="text" placeholder="Your email address..."/>
+                <button className="btn-cus" onClick={handleClick} type="submit">Notify Me</button>
+                <span className="error-msg">{infoText}</span>
             </form>
+            
         </main>
         
     );
